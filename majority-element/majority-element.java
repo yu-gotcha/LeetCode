5c12e70ccc6d82d5289a5481ebf6ef9ddf81e653
Munsu Yu <yu.gotcha@gmail.com>
2021-11-09 22:24:37 +0900

    Time: 1 ms (99.90%), Space: 42.1 MB (94.32%) - LeetHub

---
+++ majority-element/majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int major = nums[0];
+        int count = 1;
+        
+        for(int i = 1; i < nums.length; i++){
+            if(count == 0) {
+                major = nums[i];
+                count = 1;
+            }
+            else if(nums[i] == major) count++;
+            else count--;
+            
+            if(count > nums.length / 2) break;
+        }
+        
+        return major;
+    }
+}
