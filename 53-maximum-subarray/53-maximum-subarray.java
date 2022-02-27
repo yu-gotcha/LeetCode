37712cbee34458e6cfe690783168b099dfdec1e1
Munsu Yu <yu.gotcha@gmail.com>
2022-02-27 12:32:23 +0900

    Time: 1 ms (100.00%), Space: 76.2 MB (45.33%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE, currentSum = 0;
+
+        for(int num : nums) {
+            currentSum += num;
+            if(currentSum > maxSum) maxSum = currentSum;
+            if(currentSum < 0) currentSum = 0;
+        }
+
+        return maxSum;
+    }
+}
