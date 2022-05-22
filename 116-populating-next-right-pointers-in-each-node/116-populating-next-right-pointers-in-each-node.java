c36468c531f4e852f9f687a781198d8d1c3657b1
Munsu Yu <yu.gotcha@gmail.com>
2022-05-22 15:52:21 +0900

    Time: 1 ms (65.71%), Space: 46.9 MB (63.01%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Node node = root;
+        
+        while(node != null) {
+            Node tmp = node;
+            
+            while(node != null && node.left != null) {
+                node.left.next = node.right;
+                node.right.next = (node.next == null ? null : node.next.left);
+                node = node.next;
+            }
+            
+            node = tmp.left;
+        }
+        
+        return root;
+    }
+}
