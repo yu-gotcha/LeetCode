43f6fc75ba1f162fd74b200e1ba4cf0fa8faa0c4
Munsu Yu <yu.gotcha@gmail.com>
2022-07-03 21:27:21 +0900

    Time: 1 ms (92.72%), Space: 40.4 MB (89.58%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() == 0) return true;
+        int index = 0;
+        for(int i = 0; i < t.length(); i++) {
+            if(t.charAt(i) == s.charAt(index)) index++;
+            if(index == s.length()) return true;
+        }
+        
+        return index == s.length() ? true : false;
+    }
+}
