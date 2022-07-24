6771b26193d7e792c8c387521e4ffa6787726700
Munsu Yu <yu.gotcha@gmail.com>
2022-07-24 21:54:37 +0900

    Time: 3 ms (55.19%), Space: 49.2 MB (60.46%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps=0, farthest=0, current=0;
+        
+        for(int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(nums[i] + i,farthest);
+            
+            if(i == current) {
+                current = farthest;
+                jumps++;
+            }
+        }
+        
+        return jumps;
+    }
+}
