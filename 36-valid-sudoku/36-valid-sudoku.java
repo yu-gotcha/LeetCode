42c8f7e8cf989e215e920b906d78489ef8d3394b
Munsu Yu <yu.gotcha@gmail.com>
2022-10-02 22:00:53 +0900

    Time: 17 ms (33.03%), Space: 46.5 MB (62.60%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -0,0 +1,54 @@
+class Solution {
+    public static final int N = 9;
+    
+    public boolean isValidSudoku(char[][] board) {
+        System.out.println(checkRow(board));
+        System.out.println(checkColumn(board));
+        System.out.println(checkSquare(board));
+        System.out.println(checkRow(board) && checkColumn(board) && checkSquare(board));
+        return checkRow(board) && checkColumn(board) && checkSquare(board);
+    }
+    
+    public boolean checkRow(char[][] board) {
+        for(int i = 0; i < N; i++) {
+            boolean[] check = new boolean[N];
+            for(int j = 0; j < N; j++) {
+                if(board[i][j] == '.') continue;
+                int num = Character.getNumericValue(board[i][j]);
+                if(check[num - 1]) return false;
+                else check[num - 1] = true;
+            }
+        }
+        return true;
+    }
+    
+    public boolean checkColumn(char[][] board) {
+        for(int j = 0; j < N; j++) {
+            boolean[] check = new boolean[N];
+            for(int i = 0; i < N; i++) {
+                if(board[i][j] == '.') continue;
+                int num = Character.getNumericValue(board[i][j]);
+                if(check[num - 1]) return false;
+                else check[num - 1] = true;
+            }
+        }
+        return true;
+    }
+    
+    public boolean checkSquare(char[][] board) {
+        for(int k = 0; k < 3; k++) {
+            for(int l = 0; l < 3; l++) {
+                boolean[] check = new boolean[N];
+                for(int i = k * 3; i < (k + 1) * 3; i++) {
+                    for(int j = l * 3; j < (l + 1) * 3; j++) {
+                        if(board[i][j] == '.') continue;
+                        int num = Character.getNumericValue(board[i][j]);
+                        if(check[num - 1]) return false;
+                        else check[num - 1] = true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
